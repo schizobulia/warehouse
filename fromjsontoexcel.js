057fe58080fc060a7df055083573cf0125477d6d
did <2833324528@qq.com>
2017-12-12 17:16:40 +0800

    add create excel script

---
+++ fromjsontoexcel.js
@@ -0,0 +1,52 @@
+/**
+ * 根据json数据 ---> 创建 ---> Excel表
+ */
+var jexcel = require('json2excel');  //创建Excel表
+var jsonFile = require('jsonfile');   //读取json数据
+var path = require('path');
+var fs = require('fs');
+
+
+/**
+ * 读取json文件
+ */
+jsonFile.readFile('filename.json', function (err, jsonData) {
+    let list = JSON.parse(jsonData);    //格式：[{name: '苛苛', sex: '男', }, {...}]
+    generationXls(userItem);  
+});
+
+/**
+ * 创建xlsx文件  并导入json数据
+ */
+function generationXls(userItem) {
+    var sheet1 = {
+        header: {
+            'name': '姓名',
+            'sex': '性别'
+
+        },
+        items: userItem,
+        sheetName: 'sheet1'  //  
+    };
+
+    var data = {
+        sheets: [sheet1],
+        filepath: path.join(path.resolve(__dirname, '../..'), "xls/j2x.xlsx")
+    }
+
+    fs.writeFile(path.join(path.resolve(__dirname, '../..'), "xls/output.txt"), "Hello World!", function (err) {
+        if (err) {
+            return console.log(err);
+        }
+        console.log("File saved successfully!");
+    });
+
+
+    jexcel.j2e(data, function (err) {
+        console.log('finish')
+    });
+
+}
+
+
+
