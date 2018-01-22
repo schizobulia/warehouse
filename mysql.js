b83d0f21e4a2dfd8ff355c55dc8000529f298883
schizobulia <15135460425@163.com>
2018-01-22 21:01:24 +0800

    增加一些mysql基本操作.

--- mysql.js
+++ mysql.js
@@ -1,16 +1,11 @@
 const mysql = require('mysql');
 
-
-/**
-    以线程池的方式去操作...
-**/
-
 const Con = mysql.createPool({
     connectionLimit: 10,
     host: 'localhost',
     user: 'root',
     password: '123456',
-    database: 'soft'
+    database: 'graduate'
 });
 
 /**
@@ -18,6 +13,7 @@ const Con = mysql.createPool({
     @sql: sql语句
     @values: 数据
 **/
+
 let query = function (sql, values) {
     return new Promise((resolve, reject) => {
         Con.getConnection(function (err, connection) {
@@ -41,95 +37,108 @@ let query = function (sql, values) {
 let value;
 
 /**
-作为数据库基类
-**/
+ * 建表
+ * @param {*} name: tableName
+ * createTable('message', map);
+ */
 
-class MySqlDataBase {
-
-    constructor(table) {
-        this.table = table;
-        this.con = Con;
-    }
-
-
-    /**
-    一个map值...
-    **/
-
-    async add(map) {
-        value = `INSERT INTO ${this.table} SET ?`;
-        // value = 'INSERT INTO ' + this.table + ' SET ?';
-        return await query(value, map);
-    }
-
-    /**
-    查询所有
-    **/
-
-    async queryAll(getResult) {
-        value = 'SELECT * FROM ' + this.table;
-    }
-
-
-
-    /**
-    field1: 新的值
-    field2: 条件
-    **/
-
-    async update(field1, field2) {
-        value = 'UPDATE ' + this.table + ' SET ' + field1 + ' = Where ' + field2 + ' = ?';
-        return await query(value);
-    }
-
-    /**
-    filed: 条件
-    **/
-
-    async del(mmap) {
-        let key;
-        for (let s in mmap) {
-            key = s;
-        }
-        value = 'DELETE FROM ' + this.table + ' WHERE ' + key + ' =  ' + mmap[key];
-        return await query(value);
-    }
-
-    /**
-        根据mmp返该对象
-        该mmap只接收一个属性
-    **/
-    async getThis(mmap) {
-        let key;
-        for (let s in mmap) {
-            key = s;
-        }
-        value = 'select * from ' + this.table + ' where ' + key + ' = "' + mmap[key] + '"';
-        let res = await query(value);
-        return res[0];
-    }
-
-
-    /**
-    查找符合条件的信息
-    **/
-    async find() {
-
-    }
-
-
-
-    /**
-    如果想重写  可以使用该方法   返回数据库对象...
-    **/
-    getCon() {
-        if (query) {
-            return query;
-        } else {
-            return null;
+async function createTable(name, clolist) {
+    value = `create table ${name} (`;
+    for (const key in clolist) {
+        if (clolist.hasOwnProperty(key)) {
+            let val = clolist[key];
+            value += `${key} ${val}`
         }
     }
+    value += ');';
+
+    let result = await query(value);
+    console.log(result);
 }
 
-// module.exports = mysql;
-module.exports = MySqlDataBase;
+
+/**
+ * 改变字段长度
+ * @param {*} tableName 
+ * @param {*} clo 
+ * @param {*} len 
+ */
+
+async function chnageCloLen(tableName, col, len) {
+    value = `alter table ${tableName}  modify column ${col} char(${len});`;
+    let result = await query(value);
+    console.log(result);
+}
+
+/**
+ * 添加一列
+ * @param {*} tableName 
+ * @param {*} col 
+ * @param {*} type 
+ * addCol('user', 'studentId', 'int(11)');
+ */
+
+async function addCol(tableName, col, type) {
+    value = `Alter table ${tableName} add column ${col} ${type}`;
+    let result = await query(value);
+    console.log(result);
+}
+
+
+/**
+ *删除一行 
+ * @param {*} mmap 
+ * delThis('user', {email: '15135460425@163.com'});
+ */
+delThis('ajunk', {title: '这是测试账号'});
+async function delThis(tableName, mmap) {
+    let key;
+    for (let s in mmap) {
+        key = s;
+    }
+    value = `DELETE FROM ${tableName} WHERE ${key} = '${mmap[key]}'`;
+    await query(value);
+    console.log('ok');
+}
+
+/**
+ * 设置唯一约束
+ * @param {*} tableName 
+ * @param {*} col 字段名
+ * constraint('user', 'email');
+ */
+
+async function constraint(tableName, col) {
+    value = `ALTER TABLE ${tableName} ADD unique(${col})`;
+    await query(value);
+    console.log('ok');
+}
+
+/**
+ * 增加一条记录
+ * @param {*} map 
+addMsg('ajunk', {title: '测试', content: '测试', userId: 'adawadczxc'});
+ */
+
+async function addMsg(tableName, map) {
+    value = `INSERT INTO ${tableName} SET ?`;
+    await query(value, map);
+    console.log('ok');
+}
+
+
+
+let map = {
+    ajunkId: 'int(11) not null',
+    // desc: 'TEXT',
+    // imgs: 'varchar',
+    // money: 'int(10) not null',
+    // time: 'Time not null',
+    // userId: 'int(11) not null',
+
+}
+
+
+// addCol('ajunk', 'star', 'int(11)');
+// createTable('message', map);
+
