aa3887f02dc86ccd1ccf767683bbe4f88f273c06
schizobulia <2833324528@qq.com>
2019-09-14 10:02:47 +0800

    添加node给送邮件(163与qq)

--- node/email.js
+++ node/email.js
@@ -1,38 +1,72 @@
+'use strict';
 const nodemailer = require('nodemailer');
+/**
+ * 测试qq邮箱
+ */
+// async function main() {
+//     let transporter = nodemailer.createTransport({
+//         service: 'qq', // 使用了内置传输发送邮件 查看支持列表：https://nodemailer.com/smtp/well-known/
+//         port: 465, // SMTP 端口
+//         secureConnection: true, // 使用了 SSL
+//         auth: {
+//             user: '2833324528@qq.com', // generated ethereal user
+//             pass: '此处为你的授权码' // generated ethereal password
+//         },
 
-const transporter = nodemailer.createTransport({
-    service: 'qq',
-    auth: {
-        user: '2833324528@qq.com',
-        pass: ''//这里为qq的密要
+//     });
 
-    }
-});
-let mailOptions = {
-    from: '2833324528@qq.com',
-    to: '452076103@qq.com', // 接受者,可以同时发送多个,以逗号隔开  
-    subject: '您好'
-};
+//     // send mail with defined transport object
+//     let info = await transporter.sendMail({
+//         from: '"Fred Foo 👻" <2833324528@qq.com>', // sender address
+//         to: '15135460425@163.com', // list of receivers
+//         subject: 'Hello ✔', // Subject line
+//         text: 'Hello world?', // plain text body
+//         html: '<b>Hello world?</b>' // html body
+//     });
+
+//     console.log('Message sent: %s', info.messageId);
+//     // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+//     // Preview only available when sending through an Ethereal account
+//     console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+//     // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+// }
+
 
 /**
- * 发送邮箱的脚本　
- * @email: 需要发送的邮箱
- * @content: 内容
+ * 163邮箱
  */
-
-function sendEmail(email, content) {
-    if (!email || !content) {
-        return 'Invalid content';
-    }
-    mailOptions.to = email;
-    mailOptions.html = `<h2>${content}</h2>`;
-    transporter.sendMail(mailOptions, function (err, info) {
-        if (err) {
-            console.log(err);
-            return;
+async function main() {
+    let transporter = nodemailer.createTransport({
+        service: '163', // 使用了内置传输发送邮件 查看支持列表：https://nodemailer.com/smtp/well-known/
+        port: 465, // SMTP 端口
+        secureConnection: true, // 使用了 SSL
+        auth: {
+            type: 'login',
+            user: 'lab5088@163.com', // generated ethereal user
+            pass: '此处为你的授权码' // generated ethereal password
+        },
+        tls: {
+            rejectUnauthorized: false
         }
-        // console.log('send email ok');
+
     });
+
+    // send mail with defined transport object
+    let info = await transporter.sendMail({
+        from: '"Fred Foo 👻" <lab5088@163.com>', // sender address
+        to: '2833324528@qq.com', // list of receivers
+        subject: 'Hello ✔', // Subject line
+        text: 'Hello world?', // plain text body
+        html: '<b>Hello world?</b>' // html body
+    });
+
+    console.log('Message sent: %s', info.messageId);
+    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+    // Preview only available when sending through an Ethereal account
+    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
 }
 
-module.exports = sendEmail;
+main().catch(console.error);
