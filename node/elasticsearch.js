a6c15a3d376de1535713f3da74f25f1ac1cb46f4
schizobulia <2833324528@qq.com>
2019-09-23 21:33:39 +0800

    refactoring  esclient

--- node/elasticsearch.js
+++ node/elasticsearch.js
@@ -1,186 +1,114 @@
+const { Client } = require('@elastic/elasticsearch');
 /**
- * 以下方法仅仅适用于脚本中使用(一些优化手段并未考虑)，真实项目中请注意关闭client。
+ * es -> 7.3.1
+ * 
+ * $ mkdir es
+ * $ cd es
+ * $ npm init -yes
+ * $ npm install @elastic/elasticsearch
+ * 
  */
-
-var elasticsearch = require('elasticsearch');
-var client = new elasticsearch.Client({
-    host: 'http://127.0.0.1:9200',
-});
-var fs = require('fs');
-
-
-/**
- * 获取文档数量
- * getNumByDocName('bank');
- * @param {*} doc_index 
- */
-async function getNumByDocName(doc_index) {
-    return await client.count({
-        index: doc_index,
-    }).catch((e) => { });
-}
-
-/**
- * 需要对数据验证
- * 获取所有数据
- * cs('bank', {sql})
- */
-async function getAll(doc_index, body) {
-    let responseQueue = [];
-    let data = [];
-    let one = await client.search({
-        size: 5000,
-        index: doc_index,
-        body: body,
-        scroll: '30s',
-    }).catch((e) => {
-        console.log(e);
-    });
-    console.log('start')
-    responseQueue.push(one);
-    let i = 0;
-    while (responseQueue.length) {
-        const response = responseQueue.shift();
-        if (data.length > 10000) {
-            i++;
-            await fs.writeFileSync(`./mdata/${i}.json`, data);
-            data = [];
-        }
-        response.hits.hits.map((e) => {
-            data.push(e);
-        });
-        if (response.hits.hits.length === 0) {
-            break;
-        }
-        let scroll_data = await client.scroll({
-            scrollId: response._scroll_id,
-            scroll: '30s',
-        }).catch((e) => { console.log(e) });
-        responseQueue.push(scroll_data);
-        scroll_data = null;
+const client = new Client({
+    node: 'http://49.234.195.116:9200',
+    auth: {
+        username: '******',
+        password: '******'
     }
-    console.log('end');
-    return 'end';
-}
+});
 
 /**
- * 查询子网ip
- * @param {*} doc_index 
+ * type：[
+ * 大佬们的博客,
+ * 插件库,
+ * 平台与工具,
+ * 日常办公,
+ * 前端,
+ * 后端,
+ * Android,
+ * 大数据,
+ * 网络安全,
+ * 设计相关,
+ * api文档,
+ * 一些好看的效果,
+ * ai,
+ * 大数据,
+ * 运维,
+ * 其他,]
  */
-async function query_subnet_ip(doc_index) {
-    const sql = { "query": { "match_phrase": { "@version": "1" } }, "aggs": { "ip_ranges": { "ip_range": { "field": "ip", "ranges": [{ "mask": "255.255.255.255/25" }] } } } };
-    return await client.search({
-        index: doc_index,
-        body: sql,
-    });
+async function main() {
+    // await add();
+    // await search();
+    // await update();
 }
 
 
 /**
- * 建议使用http去创建
- * @param {*} _index 
- * @param {*} data 
+ * 添加数据
  */
-async function createIndex(_index, data) {
-    return await client.create({
-        index: _index,
-        type: '_doc',
-        number_of_shards: 1,
-        number_of_replicas: 5,
-        id: '10',
-        body: data,
-    });
-}
-
-/**
- * 插入数据
- * @param {*} _index 
- * @param {*} data 
- */
-function inster(_index, data, type) {
-    client.bulk({
-        body: [
-            { index: { _index: _index, _type: type, _id: new Date().getTime() } },
-            { field1: data },
-        ]
-    }, function (err, resp) {
-        if (err) {
-            console.log(err, 'err');
-        }
-        console.log(resp);
-    });
-}
-
-/**
- * 是否存在
- * @param {*} _index 
- * @param {*} sql 
- */
-async function isExties(_index, sql) {
-    const sql = { "query": { "bool": { "must": [{ "term": { "name": 'datian' } }, { "term": { "password": '123123asdasd123aaab' } }] } } };
-    return await client.search({
-        index: _index,
-        body: sql,
-    });
-}
-
-/**
- * 按月查询次数
- */
-async function getDataByTime() {
-    const sql = {
-        "size": 0,
-        "query": {
-            "match_phrase": {
-                "dstIP": {
-                    "query": "59.252.32.235"
-                }
-            }
-        },
-        "aggs": {
-            "sales": {
-                "date_histogram": {
-                    "field": "@timestamp",
-                    "interval": "month",
-                    "format": "yyyy-MM-dd"
-                }
-            }
-        }
+async function add() {
+    let object = {
+        t: '记录您的东西从未如此简单！',
+        l: 'https://www.docz.site',
+        d: '文件,记录,MD,站点',
+        type: '日常办公'
     };
-    return await client.search({
-        index: 'dns_log-2017040316',
-        body: sql,
+    let result = await client.create({
+        id: new Date().getTime(),
+        index: 'warehouse',
+        type: '_doc',
+        body: object
     });
+    console.log(result);
+    await close();
 }
 
+
 /**
- * 更新doc根据query
+ * 查询数据
  */
-async function updateByQuery() {
-    return await client.updateByQuery({
-        index: 'sess*',
+async function search() {
+    let result = await client.search({
+        index: 'warehouse',
+        type: "_doc",
         body: {
             "query": {
-                "bool": {
-                    "filter": [
-                        {
-                            "terms": {
-                                "name": ['a', 'b']
-                            }
-                        },
-                    ]
+                "query_string": {
+                    "query": "开源免费的在线教育系统"
                 }
-            },
-            "script": {
-                "source": "ctx._source['isthreat']='1';"
             }
         }
-    }).catch((e) => console.log(e));
+    });
+    console.log(result);
+    await close();
 }
 
-function main() {
 
+/**
+ * 更新数据
+ */
+async function update() {
+    let result = await client.update({
+        index: 'warehouse',
+        type: '_doc',
+        id: '1568962420601',
+        body: {
+            doc: {
+                l: 'https://github.com/Qsnh/meedu'
+            }
+        }
+    })
+    console.log(result.body.hits.hits);
+    await close();
 }
 
+
+
+
+/**
+ * 关闭客户端
+ */
+async function close() {
+    await client.close();
+}
 
 main();
